ae9de532197b7f954f651a81941f7b30cbe4af1d
Zach Collins <collinzy@mail.uc.edu>
2016-09-19 01:24:36 -0400

    Implement solver for one instance

--- src/dcv/Solver.java
+++ src/dcv/Solver.java
@@ -15,4 +15,22 @@ public class Solver {
 		// Do all possible scenarios
 		// This part should use multithreading
 	}
+	
+	// Solve one specific instance of the circuit
+	// If the array of booleans is longer than the array of inputs, the remaining booleans are ignored
+	// If the array of booleans is shorter, the remaining inputs are set to false
+	boolean solveInstance(boolean[] states) {
+		int i;
+		for (i = 0; i < states.length; i++) {
+			if (i == inputs.size()) break;
+			inputs.get(i).signal = states[i];
+		}
+		if (i < inputs.size()) {
+			for (; i < inputs.size(); i++) {
+				inputs.get(i).signal = false;
+			}
+		}
+		
+		return circuit.top.isActive();
+	}
 }
