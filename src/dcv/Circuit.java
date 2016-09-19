d07d6be967bf43eb50f4de58d6a28b6ac4a53415
Zach Collins <collinzy@mail.uc.edu>
2016-09-18 22:50:56 -0400

    Create some classes

---
+++ src/dcv/Circuit.java
@@ -0,0 +1,9 @@
+package dcv;
+
+class Circuit {
+	// Notes to self:
+	// Best way to do this: make the circuit modular
+	// Circuit owns a binary/ternary tree of circuit elements
+	// Circuit structure has one parent gate, can add capability for multiple parents later
+	// Probably best if this is a doubly linked tree
+}
---
+++ src/dcv/DigitalCircuitUI.java
@@ -0,0 +1,15 @@
+package dcv;
+
+public class DigitalCircuitUI {
+	
+	public static void DoUI() {
+		
+	}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(
+				new Runnable() { public void run() {DoUI();} });
+
+	}
+
+}
