953c55f15b59049a457eb10d933a7b6a9fdb5d91
Zach Collins <collinzy@mail.uc.edu>
2016-09-21 21:46:59 -0400

    Add circuit documentation

--- src/dcv/Circuit.java
+++ src/dcv/Circuit.java
@@ -10,10 +10,12 @@ class Circuit {
 	Solver solver;
 	ArrayList<Input> inputs;
 	
+	// Return whether top gate is active
 	boolean isActive() {
 		return top.isActive();
 	}
 	
+	// Add a solver to the circuit. Should be invoked when a solution is requested.
 	void giveSolver() {
 		solver = new Solver(this);
 	}
