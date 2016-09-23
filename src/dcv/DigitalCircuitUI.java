2feb4f3108fc7c740a13baad5c0ca6165df63daa
Laura Tebben <ltebben09@gmail.com>
2016-09-23 19:54:14 -0400

    Initialized JFrame

--- src/dcv/DigitalCircuitUI.java
+++ src/dcv/DigitalCircuitUI.java
@@ -14,8 +14,14 @@ public class DigitalCircuitUI {
 		// A Graphics2D might be the best way to implement this. We'll have to draw lines
 		// to represent wires connecting circuit components.
 		
-		// Maybe check out the "Placeable" object as well.
-		
+		// Maybe check out the "Placeable" object as well
+		JFrame frame = new JFrame("Circuit Visualizer");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel emptyLabel = new JLabel("");
+		frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
