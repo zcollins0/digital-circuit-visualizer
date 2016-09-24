851e3f38614f131753ec99ba9b896d5851d6deb3
Zach Collins <collinzy@mail.uc.edu>
2016-09-24 12:55:04 -0400

    Add notes about unit tests

--- src/dcvUnitTests/UnitTests.java
+++ src/dcvUnitTests/UnitTests.java
@@ -4,6 +4,10 @@ public class UnitTests {
 
 	public static void main(String[] args) {
 		// TODO: Write all unit tests
+		
+		// Should have one unit test for each type of gate.
+		// One unit test for solveAll()
+		// One for making sure exceptions work correctly
 
 	}
 
