7d22f427cbb648f00075da0acbcc1ce09ecef2db
Laura Tebben <tebbenla@mail.uc.edu>
2016-10-27 16:15:22 -0400

    I made some changes to Popup Listener but then deleted them so it should be the same

--- src/dcv/PopupListener.java
+++ src/dcv/PopupListener.java
@@ -4,7 +4,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class PopupListener extends MouseAdapter{
-	
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if(e.getButton() == MouseEvent.BUTTON3)
